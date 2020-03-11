7317387a704353f571b8d413089d4098ff2898cf
Clemens Schmid <clemens@nevrome.de>
2020-03-11 13:10:51 +0100

    first draft of a travis setup to move files from poseidon-schema to poseidon-framework.github.io

---
+++ _deploy_schema_to_homepage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# git config
+git config --global user.email "clemens@nevrome.de"
+git config --global user.name "Clemens Schmid"
+
+# clone homepage repo
+cd ..
+git clone https://${GITHUB_PAT}@github.com/poseidon-framework/poseidon-framework.github.io.git
+
+# copy files
+cp poseidon-schema/terms.rdf.json poseidon-framework.github.io
+
+# push changes to homepage repo
+cd poseidon-framework.github.io
+git add --all *
+git commit -m "Update the schema"
+git push -q origin master
+
